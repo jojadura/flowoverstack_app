1d640ce8cd17c04ab18e1d0fe5b1eeb7ba6d3689
John Jairo <jojadura@hotmail.com>
2016-03-04 15:59:05 -0500

    Ajuste en modelo

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,7 @@ class VotesController < ApplicationController
 
     def create
         @question = Question.find(params[:question_id])
+        @question.user = current_user
         @vote = @question.votes.create
        	redirect_to @question
     end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -13,4 +13,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
+  has_many :votes, as: :voteable
 end
