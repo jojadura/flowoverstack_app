1031bd069dbb37b22558dbdb39a7e53518f6691f
John Jairo <jojadura@hotmail.com>
2016-03-06 17:49:27 -0500

    Ajuste en funcionalidad de Votos

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,8 @@ class VotesController < ApplicationController
 
     def create
         @question = Question.find(params[:question_id])
-        @question.user = current_user
-        @vote = @question.votes.create
+        @vote = @question.votes.create(user_id: current_user.id)
        	redirect_to @question
     end
-
 
 end
